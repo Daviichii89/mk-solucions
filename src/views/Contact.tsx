d4de3d82e26237fb8b11c7f773a05bc5699e19ce
Daviichii89 <davichi89@gmail.com>
2023-10-19 00:14:27 +0200

    Fix errors in the containers of each section & change font-family

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -3,22 +3,26 @@ import styles from './Contact.module.css'
 export const Contact = () => {
   return (
     <section id="Contact" className={styles.contact}>
-      <h2>Contacto</h2>
-      <form 
-        className={styles.contactForm}
-        action="https://formsubmit.co/davichi89@gmail.com"
-        method="POST"
-      >
-        <label htmlFor="name" className={styles.contactLabel}>Nombre</label>
-        <input id="name" name="name" type="text" className={styles.contactInput} />
-        <label htmlFor="email" className={styles.contactLabel}>Email</label>
-        <input id="email" name="email" type="email" className={styles.contactInput} />
-        <label htmlFor="subject" className={styles.contactLabel}>Asunto</label>
-        <input id="subject" name="subject" type="text" className={styles.contactInput} />
-        <label htmlFor="message" className={styles.contactLabel}>Mensaje</label>
-        <textarea id="message" name="message" className={styles.contactTextArea}></textarea>
-        <button type="submit" className={styles.contactButton}>Enviar</button>
-      </form>
+      <article className={styles.mainContainer}>
+        <header>
+          <h2>Contacto</h2>
+        </header>
+        <form 
+          className={styles.contactForm}
+          action="https://formsubmit.co/davichi89@gmail.com"
+          method="POST"
+        >
+          <label htmlFor="name" className={styles.contactLabel}>Nombre</label>
+          <input id="name" name="name" type="text" className={styles.contactInput} />
+          <label htmlFor="email" className={styles.contactLabel}>Email</label>
+          <input id="email" name="email" type="email" className={styles.contactInput} />
+          <label htmlFor="subject" className={styles.contactLabel}>Asunto</label>
+          <input id="subject" name="subject" type="text" className={styles.contactInput} />
+          <label htmlFor="message" className={styles.contactLabel}>Mensaje</label>
+          <textarea id="message" name="message" className={styles.contactTextArea}></textarea>
+          <button type="submit" className={styles.contactButton}>Enviar</button>
+        </form>
+      </article>
     </section>
   )
 }
--- src/views/Services.tsx
+++ src/views/Services.tsx
@@ -4,22 +4,23 @@ import styles from './Services.module.css'
 export const Services = () => {
   return (
     <section id="Services" className={styles.services}>
-      <p>Estos son nuestros servicios</p>
-      <div className={styles.container}>
-        <ul className={styles.servicesList}>
-          <li>- Reformas de cocinas y baños de diseño.</li>
-          <li>- Renovación de suelos y paredes con materiales de alta calidad.</li>
-          <li>- Instalación de sistemas de calefacción y aire acondicionado eficientes.</li>
-          <li>- Carpintería a medida: muebles, puertas y armarios.</li>
-          <li>- Electricidad y fontanería certificadas.</li>
-          <li>- Aislamiento térmico y acústico para un mayor confort.</li>
-          <li>- Pintura y decoración interior y exterior.</li>
-        </ul>
-        <picture className="swiper-container">
-          <SwiperComponent />
-        </picture>
-        
-      </div>
+      <article className={styles.mainContainer}>
+        <p>Estos son nuestros servicios</p>
+        <div className={styles.container}>
+          <ul className={styles.servicesList}>
+            <li>Reformas de cocinas y baños de diseño.</li>
+            <li>Renovación de suelos y paredes con materiales de alta calidad.</li>
+            <li>Instalación de sistemas de calefacción y aire acondicionado eficientes.</li>
+            <li>Carpintería a medida: muebles, puertas y armarios.</li>
+            <li>Electricidad y fontanería certificadas.</li>
+            <li>Aislamiento térmico y acústico para un mayor confort.</li>
+            <li>Pintura y decoración interior y exterior.</li>
+          </ul>
+          <picture className="swiper-container">
+            <SwiperComponent />
+          </picture>
+        </div>
+      </article>
     </section>
   )
 }
